55c39d26bbbea023a9db71211281214df34f8d9e
S185439 <113907421+S185439@users.noreply.github.com>
2024-10-06 16:01:21 +0200

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using MauiApp1.ViewModels;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Hosting;
+
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ MauiApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel() => Items = new ObservableCollection<string>();
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void add()
+    {   
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+        Items.Add(Text);
+        Text = string.Empty;
+    }
+}
+
